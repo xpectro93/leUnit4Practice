f52836b5ba71f4de4c218efe7fe3071d6c00be4e
xpectro93 <jonathanandrade@pursuit.org>
2020-03-19 02:02:40 -0400

    add video functionality

--- src/components/video/Video.js
+++ src/components/video/Video.js
@@ -1,19 +1,23 @@
 import React from 'react';
 
 
-const Video = ({video}) => (
+const Video = ({video}) => {
+
+  debugger
+  return (
+  
   <div key ={video.snippet.title}className={'vid-container'}>
       <h3>{video.snippet.title}</h3>
       <embed 
         Secure
         SameSite="None"
         className={'video'} 
-        src={`https://www.youtube.com/embed/${video.id.videoId}`}  
+        src={`https://www.youtube.com/embed/${video.id}`}  
         scale="aspect" 
         controller="true"
       />
       <div>{new Date(video.snippet.publishedAt).toDateString()}<p>{video.snippet.description}</p></div>
- </div>)
+ </div>)}
   
 
 
--- src/components/video/VideoPage.js
+++ src/components/video/VideoPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import secret from '../../secret.json';
 import { useParams } from 'react-router-dom';
-
+import axios from 'axios';
 
 import Video from './Video';
 import Comment from './Comment';
@@ -9,10 +9,19 @@ import Comment from './Comment';
 
 const VideoPage = props => {
   const { id } = useParams();
-  let vidObj = secret.test_query.items.filter(vid => vid.id.videoId === id);
-
+  const [ vidData, setVidData ] = React.useState([]);
+  // let vidObj = secret.test_query.items.filter(vid => vid.id.videoId === id);
+  React.useEffect(()=> {
+    const fetchVideo = async _ => {
+      let vid = await axios.get(`https://www.googleapis.com/youtube/v3/videos?id=${id}&key=${secret.api_key}&part=snippet`);
+      setVidData(vid.data.items)
+        debugger
+    }
+    fetchVideo()
+  },[id])
   return (
-    <Video video={vidObj[0]}/>
+    vidData.length ? <Video video={vidData[0]}/> :<div>Something went horribly wrong, leave while you can</div>
+      
   )
 
 }
